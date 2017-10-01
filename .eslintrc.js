92879db582956dbbfbf81da041d856a923aa3d2a
Lam Bui <lamdbui@gmail.com>
2017-10-01 14:45:53 -0700

    Add initial guidelines for AirBnB style and linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,13 @@
  *   https://github.com/reactorcore/eslint-config-hackreactor
  */
 
+// module.exports = {
+//   extends: './node_modules/eslint-config-hackreactor/index.js'
+// };
+
+
+// Use this file as a starting point for your project's .eslintrc.
+// Copy this file, and add rule overrides as needed.
 module.exports = {
-  extends: './node_modules/eslint-config-hackreactor/index.js'
-};
+  "extends": "airbnb"
+}
