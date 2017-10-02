580e058813c04704033207149d8772c3d6e5c667
Adrian Humphrey <adrianhumphrey374@gmail.com>
2017-10-02 12:54:34 -0700

    Created player class to be instantiated upon every succesfull join through socket.

---
+++ game/player.js
@@ -0,0 +1,8 @@
+//Class to be instantiated upon every new connection with server from clients seeking to 
+//join the game
+
+class Player{
+    constructor(){
+        this.username = null;
+    }
+}
