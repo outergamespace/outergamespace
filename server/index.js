7f2b985a05f686fbef35d7bf8822486ec49ed5a4
Adrian Humphrey <adrianhumphrey374@gmail.com>
2017-10-01 17:06:26 -0700

    Added a /join route for clients to visit in order to join the current game.

--- server/index.js
+++ server/index.js
@@ -4,8 +4,14 @@ var io = require('socket.io')(server);
 
 server.listen(8080);
 
+//Main web page for the host (projector)
 app.get('/', function (req, res) {
   res.send('Hello From Outer Game Space! \n So nice to meet you!');
+});
+
+//Main page for clients to join current game
+app.get('/join', function (req, res) {
+    res.send('Hello Client ! \n Please press button below to join the game');
 });
 
 io.on('connection', function (socket) {
