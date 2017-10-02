5329d8a01ffda6bf5d17ca6dd6845a6089c7f57c
Leo Leung <leungleoqin@gmail.com>
2017-10-02 15:59:49 -0700

    Implement function to fetch questions from database

--- test/DatabaseSpec.js
+++ test/DatabaseSpec.js
@@ -1,10 +1,70 @@
 /* eslint-env mocha */
 const expect = require('chai').expect;
+const db = require('../db/index.js');
 
 describe('Database', () => {
   describe('[REPLACE ME] test template', () => {
     it('[REPLACE ME] should return true for matching "test template" text', () => {
       expect('test template').to.equal('test template');
+    });
+  });
+
+  describe('Retrieve questions from database', () => {
+    it('Should retrieve n questions when n is less than the number of items in the database', (done) => {
+      db.getQuestions(3)
+        .then((results) => {
+          expect(results.length).to.equal(3);
+        })
+        .finally(() => {
+          done();
+        });
+    });
+
+    it('Should retrieve 5 questions when n is not provided', (done) => {
+      db.getQuestions()
+        .then((results) => {
+          expect(results.length).to.equal(5);
+        })
+        .finally(() => {
+          done();
+        });
+    });
+
+    it('Should return a question object', (done) => {
+      db.getQuestions()
+        .then((results) => {
+          const question = results[0];
+          expect(question).to.have.property('question');
+          expect(question).to.have.property('correct_ans');
+          expect(question).to.have.property('category');
+          expect(question).to.have.property('difficulty');
+          expect(question).to.have.property('incorrect_ans_1');
+          expect(question).to.have.property('incorrect_ans_2');
+          expect(question).to.have.property('incorrect_ans_3');
+        })
+        .finally(() => {
+          done();
+        });
+    });
+
+    // NOTE: this test has a 1/120 (5!) chance of failing if there are 5 questions in the db
+    it('Should return random questions every time', (done) => {
+      let results1;
+      let results2;
+      db.getQuestions()
+        .then((results) => {
+          results1 = results;
+        })
+        .then(() => db.getQuestions())
+        .then((results) => {
+          results2 = results;
+        })
+        .then(() => {
+          expect(results1).to.not.eql(results2);
+        })
+        .finally(() => {
+          done();
+        });
     });
   });
 });
