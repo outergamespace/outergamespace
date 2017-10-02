dfd784724fe830295c89ac55b1b37605fe0f2a80
Lynne Daniels <lynne33477@gmail.com>
2017-10-02 14:00:37 -0700

    App and PreGame components render

--- client/src/components/presenter/PreGame.jsx
+++ client/src/components/presenter/PreGame.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
-//import /*component name*/ from './childname.jsx'; if needed
+import PlayerList from '../common/PlayerList.jsx';
+// import /*component name*/ from './childname.jsx'; if needed
 
 // render venue screen with list of joined players and a start button
 
-const PreGame = (props) => (
-  <div>
-
-  </div>
-);
+class PreGame extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inPlay: true,
+    };
+    this.startNewGame = this.startNewGame.bind(this);
+  }
+  startNewGame() {
+    console.log('clicked');
+    this.setState({
+      // TODO start game code
+    });
+  }
+  render() {
+    return (
+      <div>
+        <div>Waiting for Players to Join</div>
+        <PlayerList players={this.props.players} />
+        <button onClick={this.startNewGame}>Start</button>
+      </div>
+    );
+  }
+}
 
 export default PreGame;
