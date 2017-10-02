251c21088798483f8a8bd4b1e9de0de22b79dc23
Lam Bui <lamdbui@gmail.com>
2017-10-01 21:57:43 -0700

    Add missing template test spec files and package-lock.json

---
+++ test/DatabaseSpec.js
@@ -0,0 +1,9 @@
+var expect = require('chai').expect;
+
+describe('Database', () => {
+  describe('[REPLACE ME] test template', () => {
+    it('[REPLACE ME] should return true for matching "test template" text', () => {
+      expect('test template').to.equal('test template');
+    });
+  });
+});
---
+++ test/ServerSpec.js
@@ -0,0 +1,9 @@
+var expect = require('chai').expect;
+
+describe('Server', () => {
+  describe('[REPLACE ME] test template', () => {
+    it('[REPLACE ME] should return true for matching "test template" text', () => {
+      expect('test template').to.equal('test template');
+    });
+  });
+});
