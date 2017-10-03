16dc6f0a6a84f8e9f54faac431f83f3b2f532ed3
Lynne Daniels <lynne33477@gmail.com>
2017-10-03 16:53:39 -0700

    add socket connection to player App componend in index.jsx

--- client/src/components/player/index.jsx
+++ client/src/components/player/index.jsx
@@ -5,11 +5,14 @@ import Wait from './Wait.jsx';
 import Question from './Question.jsx';
 import Finish from './Finish.jsx';
 
+const io = require('socket.io-client');
+
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { screen: 'Join' };
     this.changePlayerStatus = this.changePlayerStatus.bind(this);
+    this.createSocketConnection = this.createSocketConnection.bind(this);
   }
   // possible states = Join, Wait, Question, Finish;
   changePlayerStatus(state) {
@@ -17,15 +20,20 @@ class App extends React.Component {
   }
 
 
-
-
-
-  
+  createSocketConnection() {
+    // make the connection
+    const connection = io.connect('http://10.6.70.110:3000');
+    // start the game
+    connection.emit('startGame');
+    connection.on('status', data => {
+      console.log('*** DATA: ', data.connection);
+    });
+    // simulate a game being joined
+    connection.emit('joinGame', { username: 'Lam' });
+    // this.setState({ socketConnection: this.createSocketConnection() });
+  }
 
   render() {
-
-    let currentScreen = '';
-
     if (this.state.screen === 'Join') {
       return <Join />;
     } else if (this.state.screen === 'Wait') {
