10ee8918fbe6f4cb11df24696b8f42611286a3eb
Lam Bui <lamdbui@gmail.com>
2017-10-02 19:26:23 -0700

    Adjust some formatting on the Answerlist and PlayerListEntry components

--- client/src/components/common/AnswerList.jsx
+++ client/src/components/common/AnswerList.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 import AnswerListEntry from './AnswerListEntry';
 
-const AnswerList = (props) => {
+const AnswerList = (props) => (
   <div className="player-list">
     {props.answers.map((answer, k) => <AnswerListEntry answer={answer} key={k} /> )}
   </div>
-};
+);
 
 export default AnswerList;
