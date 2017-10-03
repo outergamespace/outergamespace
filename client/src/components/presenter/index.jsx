37459934850523192aacb3303b0712bbfb17fe38
Lynne Daniels <lynne33477@gmail.com>
2017-10-02 23:53:40 -0700

    refactor player info into rows, update webpack -dev to monitor css changes

--- client/src/components/presenter/index.jsx
+++ client/src/components/presenter/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PreGame from './PreGame.jsx';
+import css from '../../../styles/style.css';
+
 
 // TODO import component files
 
