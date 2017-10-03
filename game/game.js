b759bbc6d4730c10fd9ee34aa12d0ffed7292919
Adrian Humphrey <adrianhumphrey374@gmail.com>
2017-10-03 11:06:32 -0700

    New Branch created in order to merge.

--- game/game.js
+++ game/game.js
@@ -1,32 +1,3 @@
-<<<<<<< HEAD
-// This is a game class, implemented right now as a Singleton
-
-// Global variable for game instance
-let gameInstance = null;
-
-class Game {
-  constructor() {
-    if (!gameInstance) {
-      gameInstance = this;
-    }
-
-    this.players = [];
-
-    return gameInstance;
-  }
-
-  addPlayer(player) {
-    this.players.push(player);
-  }
-
-  endGame() {
-    gameInstance = null;
-  }
-}
-
-module.exports.Game = Game;
-module.exports.gameInstance = gameInstance;
-=======
 //This is a game class, implemented right now as a Singleton
 
 //Global variable for game instance
@@ -55,4 +26,3 @@ class Game {
 
 module.exports.Game = Game;
 module.exports.gameInstance = gameInstance;
->>>>>>> 57383501d9f8522fcd7c930ea92ce6cdcb7e7ccc
