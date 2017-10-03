d836ec4b0c0dbc002b593f0a0113339bc6f256fb
Lynne Daniels <lynne33477@gmail.com>
2017-10-03 13:37:45 -0700

    player screen components all display on player/index.jsx

--- client/src/components/player/Question.jsx
+++ client/src/components/player/Question.jsx
@@ -5,11 +5,46 @@ class Question extends React.Component {
     super(props);
     //this.state =
   }
-  render () {
+  render() {
     return (
+      <div className="container">
+        <div className="row">
+          <div >{this.props.question}</div>
+          <div>Put Timer Here</div>
+        </div>
+        <div className="row">
+          
+            <form className="row-item" action="">
+              <div className="answer">
+                <label htmlFor="answer1" className="row">
+                  <input type="radio" name="answer" id="answer1" />
+                  {this.props.answers[0]}
+                </label>
+                <label htmlFor="answer2" className="row">
+                  <input type="radio" name="answer" id="answer2" />
+                  {this.props.answers[1]}
+                </label>
+                <label htmlFor="answer3" className="row">
+                  <input type="radio" name="answer" id="answer3" />
+                  {this.props.answers[2]}
+                </label>
+                <label htmlFor="answer4" className="row">
+                  <input type="radio" name="answer" id="answer4" />
+                  {this.props.answers[3]}
+                </label>
+              </div>
+              <button>Submit</button>
+            </form>
+            <div className="hide-on-player row-item">
+              answered player checklist goes here
+            </div>
+          
+        </div>
 
-    )
+      </div>
+
+    );
   }
 }
 
-export default Question
+export default Question;
--- client/src/components/player/index.jsx
+++ client/src/components/player/index.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Join from './Join.jsx';
+import Wait from './Wait.jsx';
+import Question from './Question.jsx';
+import Finish from './Finish.jsx';
 
 // TODO import component files
 
-const App = () => (
+const App = (props) => (
   <div>
     <h3>INSERT PLAYER CLIENT COMPONENT HERE</h3>
+    <Join/>
+    <Wait/>
+    <Question question={props.question} answers={props.answers}/>
+    <Finish/>
   </div>
 );
-
-ReactDOM.render(<App />, document.getElementById('app'));
+const testQuestion = 'This is a test Question';
+const testAnswers = [
+  'Here is a test answer',
+  'Here is second test answer',
+  'Here is third test answer',
+  'Here is fourth test answer',
+];
+const testPlayer = {
+  username: 'presenter',
+  score: 7,
+};
+ReactDOM.render(<App player={testPlayer} answers={testAnswers} question={testQuestion} />, document.getElementById('app'));
