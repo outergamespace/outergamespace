b95d7a6f5edb62de49468dff5a6590920f872a67
Lam Bui <lamdbui@gmail.com>
2017-10-04 16:01:11 -0700

    Add documentation for the Game class

--- game/game.js
+++ game/game.js
@@ -12,7 +12,14 @@ const scrambleAnswers = question => (
   ])
 );
 
+/**
+ * A module that can create and provides an interface to a Game object
+ * @module game
+ */
+
+/** Class representing a Game class */
 class Game {
+  /** Create a game instance */
   constructor() {
     this.players = {};
     db.getQuestions()
@@ -23,6 +30,9 @@ class Game {
     this.answeredCount = 0;
   }
 
+  /**
+   * Restart the game
+   */
   restart() {
     this.players = {};
     this.questions = db.getQuestions();
@@ -30,10 +40,20 @@ class Game {
     this.answeredCount = 0;
   }
 
+  /**
+   * Checks to see if player is already in the game
+   * @param {Object} player - the player object to check for
+   * @return {boolean} if the player is already in the game
+   */
   hasPlayer(player) {
     return Object.prototype.hasOwnProperty.call(this.players, player.username);
   }
 
+  /**
+   * Adds a player to the game
+   * @param {Object} player - the player object to add to the current game
+   * @throws {Error} Will throw an error if the given username has already been taken
+   */
   addPlayer(player) {
     if (this.hasPlayer(player)) {
       throw new Error('Username already taken');
@@ -42,6 +62,10 @@ class Game {
     }
   }
 
+  /**
+   * Retrieves the next question to be used in the game
+   * @return {Object} containing a 'prompt' and a set of 'answers'
+   */
   nextQuestion() {
     this.answeredCount = 0;
     const question = this.questions[this.currentQuestion];
@@ -50,11 +74,23 @@ class Game {
     return { prompt, answers };
   }
 
+  /**
+   * Checks an answer from the player to verify if it matches the correct answer
+   * for the current question prompt
+   * @param {string} answer - the answer string selected by the player
+   * @return {boolean} if the given answer matches the correct answer
+   */
   checkAnswer(answer) {
     const correctAns = this.questions[this.currentQuestion].correct_ans;
     return correctAns === answer;
   }
 
+  /**
+   * Receives an answer from a given user, increments the submit counter, and
+   * calculates the user's score
+   * @param {string} username - the username of the player
+   * @param {string} answer - the given answer of the user
+   */
   receiveAnswer(username, answer) {
     this.answeredCount += 1;
     if (this.checkAnswer(answer)) {
@@ -62,14 +98,24 @@ class Game {
     }
   }
 
+  /**
+   * Checks to see if all players have submitted their answers
+   * @return {boolean} if all players have submitted their answers
+   */
   allAnswered() {
     return this.answeredCount === Object.keys(this.players).length;
   }
 
+  /**
+   * Gets the the player Objects and sorts them in descending order
+   * @return {Array} all player Objects sorted in descending order
+   */
   getScores() {
     return _.sortBy(_.values(this.players), 'score');
   }
 }
 
+/** creates a new Game object */
 module.exports = new Game();
+/** a Game object */
 module.exports.Game = Game;
