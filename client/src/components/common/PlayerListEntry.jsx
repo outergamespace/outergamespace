941bdc3c275417d6c815d09522041ef50a330613
Lam Bui <lamdbui@gmail.com>
2017-10-03 18:05:35 -0700

    Enable listener for players joining presenter screen

--- client/src/components/common/PlayerListEntry.jsx
+++ client/src/components/common/PlayerListEntry.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const PlayerListEntry = (props) => {
   return (
     <div className="player-list-entry row">
-      <div className="player-list-entry-avatar">{props.player.avatar}</div>
-      <div className="player-list-entry-name">{props.player.name}</div>
+      {/* <div className="player-list-entry-avatar">{props.player.avatar}</div> */}
+      <div className="player-list-entry-name">{props.player.username}</div>
       <div className="player-list-entry-score">{props.player.score}</div>
     </div>
   );
