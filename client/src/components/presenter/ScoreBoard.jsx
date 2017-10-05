388fc04607749c4461f9d3d24d03b1e00f3505ec
Leo Leung <leungleoqin@gmail.com>
2017-10-04 18:22:52 -0700

    Implement socket handlers and listeners on presenter

--- client/src/components/presenter/ScoreBoard.jsx
+++ client/src/components/presenter/ScoreBoard.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 import ScoreBoardEntry from './ScoreBoardEntry.jsx';
 
-const ScoreBoard = props => (
+const propTypes = {
+  players: PropTypes.arrayOf(PropTypes.object).isRequired,
+  final: PropTypes.bool,
+  restartGame: PropTypes.func,
+};
+
+const defaultProps = {
+  final: false,
+  restartGame: () => {},
+};
+
+const ScoreBoard = ({ players, final, restartGame }) => (
   <div className="score-board">
-    <h3 className="score-board-title">Final Results</h3>
+    <h3 className="score-board-title">
+      {final ? 'Final' : ''} Results
+    </h3>
     <table>
-      {props.players.map(player => (<ScoreBoardEntry player={player} />))}
+      {players.map(player => (<ScoreBoardEntry player={player} />))}
     </table>
+    {final ? <button onClick={restartGame} >Start New Game</button> : ''}
   </div>
 );
+
+ScoreBoard.propTypes = propTypes;
+ScoreBoard.defaultProps = defaultProps;
 
 export default ScoreBoard;
