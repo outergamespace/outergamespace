c4900582c6cad2b0231499b8a41b0dd9f01d1b8b
Leo Leung <leungleoqin@gmail.com>
2017-10-07 12:15:27 -0700

    Add additional tests for Player class

--- test/PlayerSpec.js
+++ test/PlayerSpec.js
@@ -3,19 +3,44 @@ const expect = require('chai').expect;
 const Player = require('../game/player.js');
 
 describe('Player', () => {
-  describe('Should create Player with username and score of zero', () => {
-    const newPlayer = new Player('testPlayer');
-    it('new player should have the correct name', () => {
-      expect(newPlayer.username).to.equal('testPlayer');
+  let player;
+
+  beforeEach(() => {
+    player = new Player('alan');
+  });
+
+  describe('Should create a new player', () => {
+    it('New player should have the correct name', () => {
+      expect(player.username).to.equal('alan');
     });
 
-    it('new player should have a score of zero', () => {
-      expect(newPlayer.score).to.equal(0);
+    it('New player should have a score of zero', () => {
+      expect(player.score).to.equal(0);
     });
 
-    it('new player should have ability to add to their score', () => {
-      newPlayer.addToScore(100);
-      expect(newPlayer.score).to.equal(100);
+    it('New player should have an answered state of false', () => {
+      expect(player.answered).to.equal(false);
+    });
+  });
+
+  describe('Should increment the score of a player', () => {
+    it('Should increment the score of a player', () => {
+      player.addToScore(10);
+      expect(player.score).to.equal(10);
+      player.addToScore(10);
+      player.addToScore(10);
+      expect(player.score).to.equal(30);
+    });
+  });
+
+  describe('Should set the answered state of a player', () => {
+    it('Should set the answered state', () => {
+      player.setAnswered(true);
+      expect(player.answered).to.equal(true);
+      player.setAnswered(false);
+      expect(player.answered).to.equal(false);
+      player.setAnswered(true);
+      expect(player.answered).to.equal(true);
     });
   });
 });
