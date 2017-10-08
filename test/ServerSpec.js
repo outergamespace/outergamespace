406b1cbc88947f405d319c278eb55721a795dac2
Leo Leung <leungleoqin@gmail.com>
2017-10-07 18:06:47 -0700

    Add tests for server

--- test/ServerSpec.js
+++ test/ServerSpec.js
@@ -1,27 +1,72 @@
 /* eslint-env mocha */
-const expect = require('chai').expect;
 const request = require('supertest');
+const app = require('../server/index.js');
+
+const PORT = 4568;
 
 describe('Server', () => {
   let server;
+
   beforeEach(() => {
-    server = require('../server/index.js');
+    server = app.listen(PORT);
   });
+
   afterEach(() => {
     server.close();
   });
 
-  describe('Should create a new server instance with one game instance', () => {
-    it('responds to /', (done) => {
+  describe('Should serve the correct client app', () => {
+    it('Should serve presenter client on GET requests to /', (done) => {
       request(server)
         .get('/')
-        .expect(200, done);
+        .expect('Content-Type', /text\/html/)
+        .expect(/src="dist\/index_presenter\.js"/)
+        .expect(200)
+        .end(done);
     });
 
-    it('404 everything else', (done) => {
+    it('Should serve player client on GET requests to /join', (done) => {
+      request(server)
+        .get('/join')
+        .expect('Content-Type', /text\/html/)
+        .expect(/src="dist\/index_player\.js"/)
+        .expect(200)
+        .end(done);
+    });
+  });
+
+  describe('Should serve static files', () => {
+    it('Should serve presenter js file', (done) => {
+      request(server)
+        .get('/dist/index_presenter.js')
+        .expect('Content-Type', /application\/javascript/)
+        .expect(200)
+        .end(done);
+    });
+
+    it('Should serve player js file', (done) => {
+      request(server)
+        .get('/dist/index_player.js')
+        .expect('Content-Type', /application\/javascript/)
+        .expect(200)
+        .end(done);
+    });
+
+    it('Should serve css file', (done) => {
+      request(server)
+        .get('/styles/style.css')
+        .expect('Content-Type', /text\/css/)
+        .expect(200)
+        .end(done);
+    });
+  });
+
+  describe('Should return 404 for non-existent routes', () => {
+    it('Should return 404 for non-existent routes', (done) => {
       request(server)
         .get('/foo/bar')
-        .expect(404, done);
+        .expect(404)
+        .end(done);
     });
   });
 });
