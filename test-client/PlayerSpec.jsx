59edb7844742c575261161bf834d79e0384fa606
Lynne Daniels <lynne33477@gmail.com>
2017-10-09 14:43:01 -0700

    clean up

--- test-client/PlayerSpec.jsx
+++ test-client/PlayerSpec.jsx
@@ -4,11 +4,7 @@ import { expect } from 'chai';
 import { sinon, spy } from 'sinon';
 import { mount, render, shallow } from 'enzyme';
 import React from 'react';
-import AnswerList from './../client/src/components/player/AnswerList';
 import AnswerListEntry from './../client/src/components/player/AnswerListEntry';
-import App from './../client/src/components/player/App';
-// TODO solve 'app is not a DOM element error'
-// import Index from './../client/src/components/player/index';
 import Join from './../client/src/components/player/Join';
 import Question from './../client/src/components/player/Question';
 import TextScreen from './../client/src/components/player/TextScreen';
@@ -16,45 +12,12 @@ import Timer from './../client/src/components/player/Timer';
 
 describe('Player', () => {
 
-/*  cannot read TypeError: Cannot read property 'map' of undefined
-
-describe('AnswerList', () => {
-    const wrapper = shallow(<AnswerList />);
-
-    xit('has three false answers', () => {
-      wrapper.setProps({
-        answers: ['apple', 'b', 'c'],
-        currentAnswer: 'a',
-        updateAnswer: () => {},
-      });
-      //expect(wrapper.find('AnswerListEntry')).to.have.length(3);
-      expect.wrapper.contains('apple').to.equal(true);
-    });
-  });*/
-
   describe('AnswerListEntry', () => {
     const wrapper = shallow(<AnswerListEntry />);
     it('renders an answer', () => {
       wrapper.setProps({ answer: 42 });
       expect(wrapper.contains(42)).to.equal(true);
     });
-  });
-
-  describe('App', () => {
-    //const wrapper = shallow(<App />);
-
-    /* it('exists', () => {
-      expect(wrapper.type()).to.equal(function);
-    }); */
-  });
-
-  describe ('Index', () => {
-    /* const wrapper = shallow(<Index />);
-
-    it('exists', () => {
-      expect(wrapper.contains('App')).to.equal(true);
-    }); */
-
   });
 
   describe('Join', () => {
@@ -94,4 +57,3 @@ describe('AnswerList', () => {
     });
   });
 });
-
--- test-client/PresenterSpec.jsx
+++ test-client/PresenterSpec.jsx
@@ -5,25 +5,16 @@ import { sinon, spy } from 'sinon';
 import { mount, render, shallow } from 'enzyme';
 import React from 'react';
 //import AnsweredPlayerList from './../client/src/components/presenter/AnsweredPlayerList';
-import AnsweredPlayerListEntry from './../client/src/components/presenter/AnsweredPlayerListEntry';
+import Timer from './../client/src/components/presenter/Timer';
 
 
 describe('Presenter', () => {
 
-  describe('AnsweredPlayerListEntry', () => {
-    const wrapper = shallow(<AnsweredPlayerListEntry player={{username: 'tester'}}/>);
+  describe('Timer', () => {
+    const wrapper = shallow(<Timer />);
+
     it('exists', () => {
-      wrapper.setProps({
-        player: {
-          username: 'player1',
-          score: 777,
-          answered: false,
-        },
-      });
-      console.log(wrapper.props);
-      expect(wrapper.type().to.equal('div'));
+      expect(wrapper.type()).to.equal('div');
     });
-
-
   });
 });
