b91d5a88dc37feff9b02932680b8acf6d24239ef
Lynne Daniels <lynne33477@gmail.com>
2017-10-08 20:21:01 -0700

    simple test of a react component passes

--- test/ClientSpec.js
+++ test/ClientSpec.js
@@ -1,10 +1,21 @@
 /* eslint-env mocha */
-const expect = require('chai').expect;
+//const expect = require('chai').expect;
+//import { expect } from 'chai';
+import React from 'react';
+import Join from './../client/src/components/player/Join';
 
 describe('Client', () => {
   describe('[REPLACE ME] test template', () => {
     it('[REPLACE ME] should return true for matching "test template" text', () => {
       expect('test template').to.equal('test template');
     });
+  });
+});
+
+describe ('Join - player', () => {
+  const wrapper = shallow(<Join />);
+
+  it ('asks users to join a room', () => {
+    expect (wrapper.contains('Join')).to.equal(true);
   });
 });
