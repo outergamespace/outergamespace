a8abc6b069e80bca48538e93e60a6be107a442c3
christine-w <christine.wong@gmail.com>
2017-10-11 11:16:47 -0700

    Update question retrieval to come from the Open Trivia DB API After agreement with the team that there is no real benefit to storing question data, all database artifacts and methods to support question retrieval from the DB have been removed. For testing/offline purposes, a stubData file has been added.

--- helpers/openTriviaDb.js
+++ helpers/openTriviaDb.js
@@ -1,6 +1,5 @@
 const request = require('request');
 const Promise = require('bluebird');
-const db = require('../db/index.js');
 
 const OPEN_TRIVIA_DB_URL = 'https://opentdb.com';
 
@@ -14,10 +13,31 @@ openTriviaDB.fetchCategories = () => {
       else resolve(JSON.parse(body).trivia_categories);
     });
   })
-    .then(categories => db.updateCategories(categories))
-    .then(() => db.getCategories())
-    // if API request fails, try falling back to what was saved in DB
-    .catch(() => db.getCategories());
+    .catch(console.error);
+};
+
+openTriviaDB.fetchQuestions = (num) => {
+  const url = `${OPEN_TRIVIA_DB_URL}/api.php?amount=${num}`;
+  return new Promise((resolve, reject) => {
+    request(url, (err, res, body) => {
+      if (err) reject(err);
+      const questions = JSON.parse(body).results.map((result) => {
+        const question = {
+          question: result.question,
+          category: result.category,
+          type: result.type,
+          difficulty: result.difficulty,
+          correct_ans: result.correct_answer,
+        };
+        [question.incorrect_ans_1,
+          question.incorrect_ans_2,
+          question.incorrect_ans_3] = result.incorrect_answers;
+        return question;
+      });
+      resolve(questions);
+    });
+  })
+    .catch(console.error);
 };
 
 module.exports = openTriviaDB;
