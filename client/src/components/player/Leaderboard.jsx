87d9119c474baf1b2ad22dd7a3633d6caec3b837
Austin Sloane <sloane.ats@gmail.com>
2017-10-12 14:45:24 -0700

    sorted entries in users list before passing them down to LeaderboardEntry.jsx

--- client/src/components/player/Leaderboard.jsx
+++ client/src/components/player/Leaderboard.jsx
@@ -4,7 +4,8 @@ import LeaderboardEntry from './LeaderboardEntry';
 const Leaderboard = props => (
   <div className={`col-sm-3 leaderboard mr-3 ${props.leaderboardRender}`}>
     <ol>
-      {props.users.map(user => <LeaderboardEntry key={user.id} user={user} />)}
+      {props.users.sort((a, b) => b.total_points - a.total_points)
+        .map(user => <LeaderboardEntry key={user.id} user={user} />)}
     </ol>
   </div>
 );
