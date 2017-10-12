89bb2cbb803eaac9a7cff57d74772c6043d62a5f
christine-w <christine.wong@gmail.com>
2017-10-12 12:28:27 -0700

    Display list of available games in the database in the lobby Games panel

--- client/src/components/player/GameList.jsx
+++ client/src/components/player/GameList.jsx
@@ -1,13 +1,33 @@
 import React from 'react';
+import GameListItem from './GameListItem';
 
 class GameList extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      games: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('/games')
+      .then(res => res.json())
+      .then((data) => {
+        this.setState({
+          games: data
+        });
+      })
+      .catch(console.error);
   }
 
   render() {
+    const gameListItems = this.state.games.map(game =>
+      <GameListItem game={game} />
+    );
+
     return (
       <div>
+        <ul>{gameListItems}</ul>
         <form>
           <button className="btn btn-light ml-1 mr-3" onClick={this.props.createGame}>Create Game</button>
           <button className="btn btn-light mr-3">Join Game</button>
